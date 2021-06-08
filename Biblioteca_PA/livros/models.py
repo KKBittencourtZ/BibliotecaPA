8a75a00d3cc4064440f40deada1becfc47143704
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-06-08 04:49:09 -0300

    did make migrations/migrate

--- Biblioteca_PA/livros/models.py
+++ Biblioteca_PA/livros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class livro(models.Model):
+    numero               = models.DecimalField(decimal_places=4.0 ,max_digits=4)
+    nome_do_livro        = models.TextField(blank=False, null=False)
+    autores              = models.TextField(default='Desconhecido')
+    situacao             = models.BooleanField()
+    cadastro_em          = models.DateTimeField(auto_now_add=True)
+    cadastrador_do_livro = models.TextField(blank=False, null=False)
+    editado_em           = models.DateTimeField(auto_now=True)
+    editor_do_livro      = models.TextField(blank=False, null=False)
