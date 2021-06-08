7b73cbd76522b4c13effb3133f81f09f35e37293
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-06-08 06:11:31 -0300

    formulário de livro feito

--- Biblioteca_PA/livros/models.py
+++ Biblioteca_PA/livros/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class livro(models.Model):
-    numero               = models.DecimalField(decimal_places=4.0 ,max_digits=4)
+    situacao_choices = (
+        'Disponível',
+        'Emprestado'
+    )
+
+    numero               = models.DecimalField(decimal_places=4.0 ,max_digits=4,primary_key=True , unique=True, blank=False, null=False)
     nome_do_livro        = models.TextField(blank=False, null=False)
     autores              = models.TextField(default='Desconhecido')
-    situacao             = models.BooleanField()
+    situacao             = models.CharField(max_length=10, choices=situacao_choices, blank=False, null=False)
     cadastro_em          = models.DateTimeField(auto_now_add=True)
     cadastrador_do_livro = models.TextField(blank=False, null=False)
     editado_em           = models.DateTimeField(auto_now=True)
