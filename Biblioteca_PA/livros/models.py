5b6827214837337b16bb29a69ff888d171b153b0
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-06-09 15:41:28 -0300

    leves alterações

--- Biblioteca_PA/livros/models.py
+++ Biblioteca_PA/livros/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.db.models.expressions import Exists
+from django.db.auth import User
 
 # Create your models here.
 class livro(models.Model):
     situacao_choices = (
-        'Disponível',
-        'Emprestado'
+        ('Free', 'Disponível'),
+        ('Unfree', 'Emprestado')
     )
 
-    numero               = models.DecimalField(decimal_places=4.0 ,max_digits=4,primary_key=True , unique=True, blank=False, null=False)
+    numero               = models.DecimalField(decimal_places=0, max_digits=4, primary_key=True, unique=True, blank=False, null=False)
     nome_do_livro        = models.TextField(blank=False, null=False)
     autores              = models.TextField(default='Desconhecido')
     situacao             = models.CharField(max_length=10, choices=situacao_choices, blank=False, null=False)
     cadastro_em          = models.DateTimeField(auto_now_add=True)
-    cadastrador_do_livro = models.TextField(blank=False, null=False)
-    editado_em           = models.DateTimeField(auto_now=True)
-    editor_do_livro      = models.TextField(blank=False, null=False)
+    cadastrador_do_livro = models.ForeignKey(User, blank=False, null=False)
+    if cadastrador_do_livro is Exists:
+        editado_em           = models.DateTimeField(auto_now=True)
+        editor_do_livro      = models.TextField(User, blank=False, null=False)
